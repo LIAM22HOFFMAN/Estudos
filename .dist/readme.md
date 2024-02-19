2edaa40bc2f68aa9a1671ee69195541ed8795bb0
Liam Hoffman <liamhoffman2022@gmail.com>
2024-02-19 11:13:35 -0300

    estudo de lógica com js

---
+++ .dist/readme.md
@@ -0,0 +1,3 @@
+Este é projeto de estudo do curso da alura "Lógica de
+programação: mergulhe em programação com java script
+ "
---
+++ app.js
@@ -0,0 +1,10 @@
+alert ('Boas vindas ao jogo do número secreto!')
+let numeroSecreto = 5;
+let chute = prompt ('Escolha um número de 1 a 10');
+
+if (chute == numeroSecreto)  {
+    console.log ('Parabéns você acertou o número secreto')}
+ 
+
+else {alert ('Que pena, você errou o número secreto')
+};
